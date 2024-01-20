e4d1faa16a455a788caffb512daee571eefee6a8
Steve Hannay <hannay.steve@googlemail.com>
2024-01-20 22:52:45 +0000

    basic formatting of table

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,30 +21,24 @@ var audioIncorrect = new Audio("./assets/sfx/incorrect.wav")
 
 
 // module variables
-var objEvents = {}               // object containing Events and their properties
-
+var objEvents = {}               // object containing Events for the Timeblocks 
+var currentHour_24format = 0
 
 // constants 
-const timesblocksArr = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]  // array of timeblocks (24 hour clock) - edit to change Scheduler
+const timesblocksArr =
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]  // array of timeblocks (24 hour clock) - edit to change Scheduler
 
 
 // // Event Listeners 
-// btnStart.addEventListener("click", startQuiz)
+$(".container").on('click', '.save-event-btn', function(event){
 
-// divChoices.addEventListener("click", function(event) {
+    // If a save event button has been clicked (a button with the class "save-event-button") Save Events for the related Timeblock
+    var btnClicked = $(event.target)
+    var selectedTimeblock = btnClicked.attr("data-timeblock")
+    saveEvent(selectedTimeblock)
+ 
+});
 
-//     var element = event.target;      
-//     if (element.matches("button") === true) {   // If the clicked element is a button
-
-//         // Get the userAnswerIndex from the "clicked" button 
-//         userAnswerIndex = element.getAttribute("data-index");
-            
-//         // Check the "user answer" (userAnswerIndex) against the "correct answer" (correctAnswerIndex)
-//         checkAnswer()
-//     }
-// })
-
-// btnSubmitHighScore.addEventListener("click", addHighScore)
 
 
 
@@ -54,6 +48,9 @@ const timesblocksArr = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]  // array of time
 init()
 
 function init(){
+
+    // Initialise
+    currentHour_24format = new Date().getHours()
 
     // Display the current Date
     $("#currentDay").text(dayjs().format("dddd D MMMM YYYY"))
@@ -67,25 +64,25 @@ function init(){
     console.log("init() completed")
 }
 
+
 // Draw a Table of Timeblocks
 function drawTableOfTimeblocks() {
 
     var tableContainer = $(".container")            // Container element - existing <div class="container"> element where table will be positioned
     
     var tableEl = $("<table>")                      // Table elemement - used to display the Work Day Scheduler
-    .addClass("table table-hover")
-    
+    .addClass("table table-hover table-bordered custom-table")
 
-
-
+  
     // Create Table Headers
     // --------------------
 
+    // create elements
     var tableHeadEl= $("<thead>")                   // Table Head element - header section of table
     var tableHeaderRowEl = $("<tr>")                // Table Head Row element
 
     var tableHeaderCellEl_Timeblock = $("<th>")     // Table Header Cell elements
-    .addClass("p-2")
+    .addClass("p-2 text-center")
     .text("Time")
     .attr("scope", "col")
 
@@ -95,7 +92,7 @@ function drawTableOfTimeblocks() {
     .attr("scope", "col")
 
     var tableHeaderCellEl_Save = $("<th>")
-    .addClass("p-2")
+    .addClass("p-2 text-center")
     .text("Save")
     .attr("scope", "col")
 
@@ -107,30 +104,39 @@ function drawTableOfTimeblocks() {
 
 
 
-    // Create Table Rows
+    // Create Table Rows for each Timeblock
     // -----------------
 
-    var tableBodyEl= $("<tbody>")                   // Table Body element - body section of table
+    var tableBodyEl= $("<tbody>")                       // Table Body element - body section of table
 
     // Repeat for Each Timeblock (ie. a timeslot that begins at a specified time)
     for (i = 0; i < timesblocksArr.length; i++) {
 
-        var tableBodyRowEl = $("<tr>")                // Table Body Row element
+        // get selected Timeblock from array
+        var selectedTimeblock = timesblocksArr[i]
 
-        var tableBodyCellEl_Timeblock = $("<td>")     // Table Body Cell elements
-        .addClass("p-2")
-        .text(i)
+        // create elements
+        var tableBodyRowEl = $("<tr>")                  // Table Body Row element
+
+        // (a) timeblock name                           // Table Body Cell elements
+        var tableBodyCellEl_Timeblock = $("<td>")       
+        .addClass("p-2 text-center")
+        .text(formatTimeblock(selectedTimeblock))       // formatTimeblock converts 24 hour clock values to 12 hour display values
         .attr("scope", "row")
     
+        // (b) event
         var tableBodyCellEl_Event = $("<td>")
         .addClass("p-2")
-        .text(i * 10)
- 
+        .text("Event " + i )
+        .css("background-color", selectEventBackgroundColour(selectedTimeblock))  // set event background according to the Timeblock time
+        
     
+        // (c) save event utton
         var tableBodyCellEl_Save = $("<td>")
-        .addClass("p-2")
-        .text(i * 100)
- 
+        .addClass("p-2 text-center save-event-btn")
+        .text("X")
+        .attr("data-timeblock", selectedTimeblock)      // store selectedTimeblock with delete event button
+
  
 
         // - append Table Body Cells to Table Body Row
@@ -166,27 +172,69 @@ function populateTableWithEvents() {
 }
 
 
-// Save an Event
-function saveAndEvent() {
+// Save an Event for a TimeBlock
+function saveEvent(timeblock) {
 
     // Save Event to Local Storage
 
     // Repopulate the Table with Events
 
-}
-
-
-// Delete an Event
-function deleteAnEvent() {
-
-    // Delete Event from Local Storage
-
-    // Repopulate the Table with Events
+    alert("Save EVENT Button click : " + timeblock)
 
 }
+
+
+
 
 
 // Utility Functions
 // -----------------
 
+// Format Timeblock 
+function formatTimeblock(timeblock) {
 
+    // Notes : 
+    // this function formats a timeblock into a 12 hour display value (eg. "9 PM", "5 PM", etc.)
+    // a timeblock is a 24 hour clock value (eg. 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, etc.)
+
+    if (timeblock < 1) {
+        // midnight
+        return "midnight"
+    }
+    else if (timeblock < 11) {
+        // before midday
+        return timeblock + " AM"
+    } 
+    else if(timeblock < 13) {
+        return timeblock + " PM"
+    }
+    else {
+        // 1pm and after
+        return (timeblock - 12) + " PM"
+    }
+
+}
+
+// Selects an Event Background Colour according to its time of day
+function selectEventBackgroundColour(timeblock) {
+
+    // Notes :
+    // this function selects a background colour for Events according to the Timeblock time
+    // a timeblock is a 24 hour clock value (eg. 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, etc.)
+
+    console.log("timeblock = " + timeblock + " AND currentHour_24format = " + currentHour_24format)
+
+    if (timeblock < currentHour_24format) {
+        // past timeblock
+        return "red"
+    }
+    else if (timeblock === currentHour_24format) {
+        // current timeblock
+        return "yellow"
+    }
+    else {
+        // future timeblock
+        return "blue"
+    }
+
+} 
