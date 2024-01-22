2a7a262d966dc6eb26d63077b293cac743cea611
Steve Hannay <hannay.steve@googlemail.com>
2024-01-22 17:59:53 +0000

    main logic complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,35 +11,27 @@
 // Define Variables and Event Listeners
 // ------------------------------------
 
-// create references to html elements (using element id's)
-var divCurrentDate = document.querySelector("#currentDay")
-
-
-// create references to audio files
-var audioCorrect = new Audio("./assets/sfx/correct.wav")
-var audioIncorrect = new Audio("./assets/sfx/incorrect.wav")
-
+// create references to html elements 
+var divCurrentDate = document.querySelector("#currentDay")  // reference by id
+var tableContainer = $(".container")                        // reference by class
 
 // module variables
-var eventsArray = []               // array containing Event objects (Timeblock and Event Description) 
+var eventsArray = []                                        // array containing Event objects (Timeblock and Event Description) 
 var currentHour_24format = 0
 
 // constants 
 const timesblocksArr =
-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]  // array of timeblocks (24 hour clock) - edit to change Scheduler
+[7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]           // array of timeblocks (24 hour clock) - edit values to change Scheduler
 
-
-// // Event Listeners 
+// Event Listeners 
 $(".container").on('click', '.save-event-btn', function(event){
 
-    // If a save event button has been clicked (a button with the class "save-event-button") Save Events for the related Timeblock
+    // If a save event button has been clicked (a button with the class "save-event-button") Save the Event for the selected Timeblock
     var btnClicked = $(event.target)
     var selectedTimeblock = btnClicked.attr("data-timeblock")
-
     saveEvent(selectedTimeblock)
  
 });
-
 
 
 
@@ -62,9 +54,6 @@ function init(){
     // Draw a Table of Timeblocks
     drawTableOfTimeblocks()
 
-    // Populate the Table with Events
-    populateTableWithEvents()
-
     console.log("init() completed")
 }
 
@@ -72,8 +61,6 @@ function init(){
 // Draw a Table of Timeblocks
 function drawTableOfTimeblocks() {
 
-    var tableContainer = $(".container")            // Container element - existing <div class="container"> element where table will be positioned
-    
     var tableEl = $("<table>")                      // Table elemement - used to display the Work Day Scheduler
     .addClass("table table-hover table-bordered custom-table")
     .css("width","100%")
@@ -108,13 +95,10 @@ function drawTableOfTimeblocks() {
 
 
     // Create a Row and Append Row to Table Header
-
     // - append Table Header Cells to Table Header Row
     tableHeaderRowEl.append(tableHeaderCellEl_Timeblock, tableHeaderCellEl_Event, tableHeaderCellEl_Save)
-
     // - append Table Header Row to Table Head
     tableHeadEl.append(tableHeaderRowEl)
-
 
 
     // Create Table Rows for each Timeblock (Hour)
@@ -122,30 +106,30 @@ function drawTableOfTimeblocks() {
 
     var tableBodyEl= $("<tbody>")                       // Table Body element - body section of table
 
-    // Repeat for Each Timeblock (ie. a timeslot that begins at a specified time)
+    // Repeat for Each Timeblock (ie. a timeslot that begins at a specified Hour)
     for (i = 0; i < timesblocksArr.length; i++) {
 
-        // get selected Timeblock from array
+        // get selected Timeblock (Hour) from the Timeblocks Array 
         var selectedTimeblock = timesblocksArr[i]
 
         // create elements
         var tableBodyRowEl = $("<tr>")                  // Table Body Row element
 
-        // (a) timeblock name                           // Table Body Cell elements
+        // (a) Timeblock Name                           // Table Body Cell elements
         var tableBodyCellEl_Timeblock = $("<td>")       
         .addClass("p-2 time-block")
         .attr("scope", "row")
-        .text(formatTimeblock(selectedTimeblock))       // formatTimeblock converts 24 hour clock values to 12 hour display values
+        .text(formatTimeblockHour(selectedTimeblock))       // formatTimeblock converts 24 hour clock values to 12 hour display values
     
-        // (b) event description
+        // (b) Event Description
         var textAreaEl = $("<textarea>")                   // use <textarea> for multi-line input
         .css("background-color", selectBackgroundColour(selectedTimeblock))  // set background according to the Timeblock time
         .css("height","50px")
         .css("width","100%")
-        .text("Event " + i )
         .attr("id", selectedTimeblock)                  // set the event descriptions id for later reference when saving changes
+        .text(getEventDescriptionFromTimeblock(selectedTimeblock))  // populate the Event description from Local Storage
 
-        // (c) save event utton
+        // (c) Save Event Button
         var tableBodyCellEl_Save = $("<td>")
         .addClass("p-2 text-center")
  
@@ -156,43 +140,28 @@ function drawTableOfTimeblocks() {
         .attr("data-timeblock", selectedTimeblock)      // store selectedTimeblock with delete event button
         //.text("Save")
  
+        // - append Button to button Cell
         tableBodyCellEl_Save.append(saveEventButtonEl)
 
 
         // Create a Row and Append Row to Table Body
-
         // - append Table Body Cells to Table Body Row
         tableBodyRowEl.append(tableBodyCellEl_Timeblock, textAreaEl, tableBodyCellEl_Save)
-
         // - append Table Row to Table Body
         tableBodyEl.append(tableBodyRowEl)
 
     }
 
     // Final Appends
-
     // - append Table Header AND Body to Table
     tableEl.append(tableHeadEl, tableBodyEl)
-
     // - append Table to Webpage (at element <div class="container">)
     tableContainer.append(tableEl)
 
 }
 
 
-// Populate the Table with Events
-function populateTableWithEvents() {
-
-    // Read Events from Local Storage
-
-    // Clear Existing Events from Table
-
-    // Populate the Table with Events
-
-}
-
-
-// Save an Event for a TimeBlock
+// Save an Event for a TimeBlock Hour
 function saveEvent(timeblock) {
 
     // Save Event to Local Storage
@@ -204,46 +173,52 @@ function saveEvent(timeblock) {
     // Update the Array of Events
 
     // - remove any existing Event for this timeslot from the Array of Events
-    for (i=0; i<eventsArray.length; i++){
-        if (eventsArray[i].Saved_timeblock == timeblock) {
+    for (var i=0; i<eventsArray.length; i++){
 
+        if (eventsArray[i].Saved_timeblock == timeblock) {
             console.log("Removing - Saved_timeblock : " + eventsArray[i].Saved_timeblock + ", Saved_eventDescription : " + eventsArray[i].Saved_eventDescription)
             eventsArray = eventsArray.slice(0, i).concat(eventsArray.slice(i+1))  // remove an element from the array by index
-  
         }
     }
 
     // - add the selected Event to the Array of Events
-    
     console.log("Adding - Saved_timeblock : " + timeblock + ", Saved_eventDescription : " + eventDescription)
+    var eventToSaveObject = { Saved_timeblock: timeblock, Saved_eventDescription: eventDescription } // create an Event object
+    eventsArray.push(eventToSaveObject) // add the Event object to the Array of Events
 
-    var eventToSaveObject = { Saved_timeblock: timeblock, Saved_eventDescription: eventDescription } // create an object
-    
-
-    eventsArray.push(eventToSaveObject) // add the object to the array
     console.log(eventsArray)
 
-    // - save the Array of Events to Local Storage
+    // Save the Array of Events to Local Storage
     localStorage.setItem("eventsArray", JSON.stringify(eventsArray))
 
-
-    // Repopulate the html Table with Events (refresh screen)
-
-
-
-//    alert("Save EVENT Button click : timeblock = " + timeblock + ", eventDesciption = " + eventDescriptions)
+    // Refresh the Webpage
+    init()
 
 }
-
-
 
 
 
 // Utility Functions
 // -----------------
 
-// Format Timeblock 
-function formatTimeblock(timeblock) {
+
+// Get the Event description (if any) for a timeblock Hour
+function getEventDescriptionFromTimeblock(timeblock) {
+
+    // Searches the Events Array (which was populated from Local Storage)
+
+    var eventDescription = ""
+
+    for (var i=0; i < eventsArray.length; i++){
+        if (eventsArray[i].Saved_timeblock == timeblock) {
+            eventDescription = eventsArray[i].Saved_eventDescription
+        }
+    }
+    return eventDescription 
+}
+
+// Format a Timeblock Hour
+function formatTimeblockHour(timeblock) {
 
     // Notes : 
     // this function formats a timeblock into a 12 hour display value (eg. "9 PM", "5 PM", etc.)
